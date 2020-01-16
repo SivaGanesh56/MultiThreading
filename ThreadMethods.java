f52d2e90f72c0a9f309c1cf346712942bb5a2667
SivaGanesh56 <siva.ganesh584@gmail.com>
2020-01-16 14:08:31 +0530

    new file

--- ThreadMethods.java
+++ ThreadMethods.java
@@ -2,38 +2,35 @@ package MultiThreading;
 import java.*;
 /// To run ur code in proper positions and check whether the thread is live or not
 
-class xyz extends Thread
-{
-    public void run()
-    {
-        for(int i=0;i<5;i++) {
-            System.out.println("Hiiii");
-            try { Thread.sleep(500);} catch (Exception e) {}
-        }
-    }
-}
-
-class abc extends Thread
-{
-    public void run()
-    {
-        for(int i=0;i<5;i++) {
-            System.out.println("Hello");
-            try { Thread.sleep(500);} catch (Exception e) {}
-        }
-    }
-}
-
 public class ThreadMethods
 {
-    public static void main(String[] args) throws Exception{
-        xyz o1 = new xyz();
-        abc o2 = new abc();
-        o1.start();
-        System.out.println(o1.isAlive());
-        o2.start();
-        o1.join();
-        o2.join();
+    public static void main(String[] args) throws Exception
+    {
+        Thread t1 = new Thread(()->
+        {
+            for(int i=0;i<5;i++) {
+                System.out.println("Hi");
+                try{
+                    Thread.sleep(500);
+                } catch (Exception e) {
+                }
+            }
+        });
+        Thread t2 = new Thread(()->
+        {
+            for(int i=0;i<5;i++)
+            {
+                System.out.println("Hello");
+                try{
+                    Thread.sleep(500);
+                } catch (Exception e) {
+                }
+            }
+        });
 
+        t1.start();
+        try{Thread.sleep(10);} catch (Exception e){}
+        t2.start();
+        System.out.println("bye");
     }
 }
